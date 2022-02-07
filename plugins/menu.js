242143b1f0fddbeb56457e39657a44b49e18355b
carlox-mass-ser <99211048+Carlox-ser-mass-ser@users.noreply.github.com>
2022-02-08 04:33:04 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,8 +10,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
-    
+    r_text[0] = "https://pin.it/7agXDA6/wp-content/uploads/CARLOX-MASS-SER-dowload.jpg";
     
     var i = Math.floor(1*Math.random())
 
